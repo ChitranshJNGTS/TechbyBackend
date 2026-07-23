d7c98c33de830f3ad130a9d76c1fa9eecdf0ae19
chitransh <chitranshjawre2001@gmail.com>
2026-07-23 17:10:33 +0530

    first commit

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { login } = require("../controllers/authController");
+const { login } = require("../controllers/AuthController");
 
 router.post("/login", login);
 
